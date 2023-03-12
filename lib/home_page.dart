60c50415b4d6dd8c1b4781faf94733271f1396d5
filip doganowski <filip.doganowski@gmail.com>
2023-03-12 20:36:12 +0100

    Create UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,6 +7,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('URL Launcher Example'),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              UrlLauncherButton(
+                text: 'Launch Email',
+              ),
+              UrlLauncherButton(
+                text: 'Launch SMS',
+              ),
+              UrlLauncherButton(
+                text: 'Launch Phone Call',
+              ),
+              UrlLauncherButton(
+                text: 'Launch https url',
+              ),
+              UrlLauncherButton(
+                text: 'Launch http url',
+              ),
+            ]),
+      ),
+    );
+  }
+}
+
+class UrlLauncherButton extends StatelessWidget {
+  const UrlLauncherButton({super.key, required this.text, this.onPressed});
+
+  final String text;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: SizedBox(width: 150, child: Center(child: Text(text))),
+    );
   }
 }
